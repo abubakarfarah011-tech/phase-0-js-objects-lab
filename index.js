1bed04364ff4597cad2be6b5c66fa16897be163f
abubakar <abubakarfarah011@gmail.com>
2025-11-23 19:53:40 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 //Write your code here
+const attendee1 = {
+  attendeeID : "T001",
+  name :" Alice Smith",
+  event : " JavaScript Confrence",
+  ticketType : "VIP",
+  ticketPrice : 150.00
+};
+
+function logAttendeeName(attendee){
+  console.log(attendee.name);
+}
+function logTicketPrice(attendee){
+  console.log(attendee.ticketPrice);
+}
+function updateTicketType(attendee,newTicketType){
+  attendee.ticketType = newTicketType;
+}
+function updateTicketPrice(attendee,newTicketPrice){
+   attendee.ticketPrice= newTicketPrice;
+}
+function removeEventProperty(attendee){
+  delete attendee.event;
+}
+function addCheckedInProperty(attendee){
+  attendee.checkedIn=true;
+}
+
+  
 
 
 
